93be4e51b3a3c198cbaae5a67cdcbc045324c1b3
Miguel Mendoza <miguel.an.mendoza@gmail.com>
2015-03-31 12:45:03 -0700

    Changed global current game var into Tkinter StringVar to update the entire game using the trace method.

--- storythegame/stg_gui.py
+++ storythegame/stg_gui.py
@@ -6,57 +6,16 @@ import sys
 import importlib
 
 import stg
+import screenplays.default_stg
 
 stg.game_mode = "GUI"
 
 FILE_NAME = ""
-CURRENT_GAME = "default_stg"
+#CURRENT_GAME = "default_stg"
 SCREENPLAY_PATH = "screenplays\\"
 
 LARGE_FONT = ("Verdana", 12)
 NORM_FONT = ("Verdana", 10)
-
-def select_game(game_choice):	
-	global CURRENT_GAME
-	
-	module_name = "screenplays." + game_choice
-	
-	# Reload if already imported
-	if module_name in sys.modules.keys():
-		# Overrides story object attribute: story_path
-		reload(sys.modules[module_name])
-		CURRENT_GAME = game_choice
-		return
-	
-	try:
-		importlib.import_module("." + game_choice, "screenplays")
-	except ImportError:
-		print "Failed to import story module"
-		return
-	
-	CURRENT_GAME = game_choice
-
-# import the default story module: default_stg
-select_game(CURRENT_GAME)
-
-def story_dict():
-	"""
-	Builds the dictionary for the screenplay files.
-	Dictionary keyed by the roomName with the file path as the value.
-	"""
-	global CURRENT_GAME
-	global SCREENPLAY_PATH
-	
-	screenplay_dir = SCREENPLAY_PATH + CURRENT_GAME[:-4]
-	
-	path_dict = {}
-	for splay_name in os.listdir(screenplay_dir):
-		splay_path = os.path.join(screenplay_dir, splay_name)
-		
-		if os.path.isfile(splay_path) and splay_name.endswith(".txt"):
-			path_dict[splay_name[:-4]] = splay_path
-	
-	return path_dict
 
 def active_games():
 	"""
@@ -104,6 +63,10 @@ class StoryTheGameApp(tk.Tk):
 		s = ttk.Style()
 		s.theme_use("clam")
 		
+		""" Track the Chosen Game """
+		self.currentGame = tk.StringVar()
+		self.currentGame.set("default_stg")
+		
 		""" Menus """
 		menubar = tk.Menu(container)
 		
@@ -113,7 +76,7 @@ class StoryTheGameApp(tk.Tk):
 		
 		for active_game in active_games():
 			selectMenu.add_command(label=active_game[:-4].capitalize(),
-									command=lambda active_game=active_game: select_game(active_game))
+									command=lambda active_game=active_game: self.select_game(active_game))
 		
 		filemenu.add_cascade(label="Select Game", menu=selectMenu)
 		filemenu.add_separator()
@@ -147,11 +110,48 @@ class StoryTheGameApp(tk.Tk):
 	def show_frame(self, cont):
 		frame = self.frames[cont]
 		frame.tkraise()
+	
+	def select_game(self, game_choice):	
+		module_name = "screenplays." + game_choice
+
+		# Reload if already imported
+		if module_name in sys.modules.keys():
+			# Overrides story object attribute: story_path
+			reload(sys.modules[module_name])
+			self.currentGame.set(game_choice)
+			return
+
+		try:
+			importlib.import_module("." + game_choice, "screenplays")
+		except ImportError:
+			print "Failed to import story module"
+			return
+
+		self.currentGame.set(game_choice)
+	
+	def story_dict(self):
+		"""
+		Builds the dictionary for the screenplay files.
+		Dictionary keyed by the roomName with the file path as the value.
+		"""
+		global SCREENPLAY_PATH
+
+		screenplay_dir = SCREENPLAY_PATH + self.currentGame.get()[:-4]
+
+		path_dict = {}
+		for splay_name in os.listdir(screenplay_dir):
+			splay_path = os.path.join(screenplay_dir, splay_name)
+
+			if os.path.isfile(splay_path) and splay_name.endswith(".txt"):
+				path_dict[splay_name[:-4]] = splay_path
+
+		return path_dict
 
 class StartPage(tk.Frame):
 	
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
+		self.controller = controller
 		
 		welcome = tk.Label(self, text="    Welcome to...\nSTORY THE GAME!", font=LARGE_FONT)
 		welcome.pack(pady=15)
@@ -160,7 +160,7 @@ class StartPage(tk.Frame):
 		buttWrapper.pack()
 		
 		button1 = ttk.Button(buttWrapper, text="Enter if you dare...",
-							command=lambda: controller.show_frame(PlayGame))
+							command=lambda: self.controller.show_frame(PlayGame))
 		button1.pack(side="left", padx = 5)
 		
 		button2 = ttk.Button(buttWrapper, text="Exit", command=quit)
@@ -170,13 +170,14 @@ class PlayGame(tk.Frame):
 	
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
+		self.controller = controller
 		
 		""" Story Printing """
 		outputFrame = tk.Frame(self)
 		outputFrame.pack(fill=tk.BOTH, expand=1)
 		
-		game_title = tk.Label(outputFrame, text="GAME TITLE HERE")
-		game_title.pack()
+		self.gameTitle = tk.Label(outputFrame, text=self.controller.currentGame.get()[:-4].capitalize())
+		self.gameTitle.pack()
 		
 		self.storyOutput = tk.Text(outputFrame, height=10, state=tk.DISABLED)
 		self.storyOutput.pack(fill=tk.BOTH, expand=1, padx=20, pady=10)
@@ -204,12 +205,19 @@ class PlayGame(tk.Frame):
 		statusBar = tk.Label(self, bd=1, text="Play Story the Game at your own risk", relief="sunken", anchor = "e")
 		statusBar.pack(side="bottom", fill="x")
 		
-	def play_game(self):
+		""" Play Game Update Function """
+		self.controller.currentGame.trace("w", callback=self.play_update)
+		
+	def clear_content(self):
 		self.storyOutput.configure(state=tk.NORMAL)
 		self.storyOutput.delete(1.0, tk.END)
 		self.storyOutput.configure(state=tk.DISABLED)
+	
+	def play_game(self):
+		self.clear_content()
 		
-		sys.modules["screenplays." + CURRENT_GAME].gameStart.story_intro()
+		current_game = self.controller.currentGame.get()
+		sys.modules["screenplays." + current_game].gameStart.story_intro()
 	
 	def gui_prompt(self, event=None):
 		if stg.current_scene:
@@ -227,11 +235,20 @@ class PlayGame(tk.Frame):
 		
 		else:
 			popup_message("Please start a new game.")
+		
+	def play_update(self, *args):
+		stg.current_scene, stg.gui_script = "", []
+		
+		self.clear_content()
+		
+		game_title = self.controller.currentGame.get()[:-4].capitalize()
+		self.gameTitle.configure(text=game_title)
 
 class StoryEdit(tk.Frame):
 	
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
+		self.controller = controller
 		
 		page_title = tk.Label(self, text="Edit Story Page", font=LARGE_FONT)
 		page_title.pack(side="top", pady=15)
@@ -253,7 +270,7 @@ class StoryEdit(tk.Frame):
 		
 		self.storyFiles = tk.StringVar()
 		self.storyFiles.set(None)
-		files = story_dict().keys()
+		files = self.controller.story_dict().keys()
 		self.storyDropDown = ttk.OptionMenu(commandWrapper, self.storyFiles,
 										self.storyFiles.get(), *files)
 		self.storyDropDown.pack(side='left', padx=5, pady=5)
@@ -264,11 +281,7 @@ class StoryEdit(tk.Frame):
 		
 		saveSceneButt = ttk.Button(commandWrapper, text="Save Scene",
 									command=self.save_script)
-		saveSceneButt.pack(side='left', padx=5, pady=5)
-		
-		refreshButt = ttk.Button(commandWrapper, text="Refresh",
-									command=self.game_update)
-		refreshButt.pack(side='left', padx=5, pady=5)
+		saveSceneButt.pack(side='left', padx=5, pady=5)		
 		
 		""" Status Bar """
 		status_text = "Edit the screenplay for each scene"
@@ -276,6 +289,9 @@ class StoryEdit(tk.Frame):
 									relief="sunken", anchor = "e")
 		# Status bar is copied for each page! TODO: Add it once for all pages!
 		self.statusBar.pack(side="bottom", fill="x")
+		
+		""" Story Edit Update Function """
+		self.controller.currentGame.trace("w", callback=self.edit_update)
 	
 	def save_script(self):
 		global FILE_NAME
@@ -299,7 +315,7 @@ Please select and open a scene from the list to view/edit"""
 		global FILE_NAME
 		
 		try:
-			FILE_NAME = story_dict()[selection]
+			FILE_NAME = self.controller.story_dict()[selection]
 		except KeyError:
 			self.statusBar.configure(text="No file selected.")
 			message = """No file selected.
@@ -318,9 +334,7 @@ Please select and open a scene from the list to view/edit."""
 		self.statusBar.configure(text="File open: " + selection + ".txt")
 		return
 	
-	def game_update(self):
-		# TODO: Use StringVar.trace(mode='w', callback=story_dict) to update the list
-		# This function is a temporary manual refresh tied to a button
+	def edit_update(self, *args):
 		global FILE_NAME
 		
 		FILE_NAME = ""
@@ -329,7 +343,7 @@ Please select and open a scene from the list to view/edit."""
 		self.storyFiles.set(None)
 		self.storyDropDown["menu"].delete(0, tk.END)
 		
-		new_files = story_dict().keys()
+		new_files = self.controller.story_dict().keys()
 		for file in new_files:
 			self.storyDropDown["menu"].add_command(label=file,
 													command=tk._setit(self.storyFiles, file))
@@ -338,6 +352,7 @@ class NewStory(tk.Frame):
 	
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
+		self.controller = controller
 		
 		page_title = tk.Label(self, text="New Story Page", font=LARGE_FONT)
 		page_title.pack(side="top", pady=15)
@@ -357,6 +372,12 @@ class NewStory(tk.Frame):
 		""" Status Bar """
 		statusBar = tk.Label(self, bd=1, text="Create your new story...", relief="sunken", anchor = "e")
 		statusBar.pack(side="bottom", fill="x")
+		
+		""" New Story Update Function """
+		self.controller.currentGame.trace("w", callback=self.new_update)
+		
+	def new_update(self, *args):
+		pass
 
 class TextRedirector(object):
 	def __init__(self, widget, tag="stdout"):
