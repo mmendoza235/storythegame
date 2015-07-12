d7cd95a4ed4b2a78971043b1c9e504371c88a128
Miguel Mendoza <miguel.an.mendoza@gmail.com>
2015-07-11 22:35:30 -0700

    Trivial Commit - Whitepace/PEP8 Guidelines

--- storythegame/stg_util.py
+++ storythegame/stg_util.py
@@ -1,35 +1,38 @@
 # keypress function modified form code.activestate.com/recipes/...
 def keypress():
-	"""
-	Waits for the user to press a key. Returns the ascii code
-	for the key pressed or zero for a function key pressed.
-	Aborts function with CTL-C
-	"""
-	import time, msvcrt
-	while 1:
-		if msvcrt.kbhit():
-			a = ord(msvcrt.getch())
-			# is it a function key?
-			if a == 0 or a == 224:
-				# discard second byte of key scan code
-				msvcrt.getch()
-				return 0
-			else:
-				return a
+    """
+    Waits for the user to press a key. Returns the ascii code
+    for the key pressed or zero for a function key pressed.
+    Aborts function with CTL-C
+    """
+    import time
+    import msvcrt
 
-		# TODO implement keypress as an animation
-		#for dot in ['.', '..', '...']:
-		#	print dot
-		#	time.sleep(0.5)
+    while 1:
+        if msvcrt.kbhit():
+            a = ord(msvcrt.getch())
+            # is it a function key?
+            if a == 0 or a == 224:
+                # discard second byte of key scan code
+                msvcrt.getch()
+                return 0
+            else:
+                return a
 
-		# TODO: Validate that time.sleep effects performance
-		# reduce CPU workload during while execution
-		time.sleep(0.5)
+        # TODO implement keypress as an animation
+        # for dot in ['.', '..', '...']:
+        # print dot
+        # time.sleep(0.5)
+
+        # TODO: Validate that time.sleep effects performance
+        # reduce CPU workload during while execution
+        time.sleep(0.5)
+
 
 def prompt(custom_prompt="--> "):
-	"""
-	Simple raw_input with custom prompt.
-	Returns the raw_input
-	"""
-	answer = raw_input(custom_prompt).lower()
-	return answer
+    """
+    Simple raw_input with custom prompt.
+    Returns the raw_input
+    """
+    answer = raw_input(custom_prompt).lower()
+    return answer
